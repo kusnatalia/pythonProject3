fc27ee515fa664700cd431deb34bcb1d3735c76f
Natalia Kuś <66189@student.ue.poznan.pl>
2023-06-02 17:09:19 +0200

    sudoku 4x4

--- solo_rekurencja.py
+++ solo_rekurencja.py
@@ -49,3 +49,67 @@ def fib(f):
 # W przeciwnym razie, zwracamy sumę dwóch poprzednich liczb ciągu Fibonacciego
 # Wywołujemy funkcję rekurencyjnie dla dwóch poprzednich liczb
 
+def rozwiaz_sudoku(sudoku):
+    if not czy_sa_puste_pola(sudoku):
+        return sudoku
+
+    m, n = znajdz_nastepne_puste_pole(sudoku)
+
+    for i in range(1, 5):
+        if czy_poprawna_liczba(sudoku, m, n, i):
+            sudoku[m][n] = i
+
+            if rozwiaz_sudoku(sudoku):
+                return sudoku
+
+            sudoku[m][n] = 0
+
+    return None
+
+
+def czy_sa_puste_pola(sudoku):
+    for row in sudoku:
+        if 0 in row:
+            return True
+    return False
+
+
+def znajdz_nastepne_puste_pole(sudoku):
+    for m in range(4):
+        for n in range(4):
+            if sudoku[m][n] == 0:
+                return m, n
+
+
+def czy_poprawna_liczba(sudoku, m, n, num):
+    # Sprawdzanie wiersza
+    for i in range(4):
+        if sudoku[m][i] == num:
+            return False
+
+    # Sprawdzanie kolumny
+    for i in range(4):
+        if sudoku[i][n] == num:
+            return False
+
+    # Sprawdzanie kwadratu 2x2
+    start_row = m - m % 2
+    start_col = n - n % 2
+    for i in range(2):
+        for j in range(2):
+            if sudoku[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+# Proces:
+# - Sprawdzamy, czy są puste pola w sudoku.
+# - Jeśli nie ma żadnych pustych pól, zwracamy rozwiązane sudoku.
+# - W przeciwnym razie, znajdujemy następne puste pole.
+# - Próujemy wstawić liczby od 1 do 4 i sprawdzamy, czy są poprawne.
+# - Jeśli liczba jest poprawna, umieszczamy ją w sudoku i rekurencyjnie wywołujemy funkcję dla pozostałych pustych pól.
+# - Jeśli otrzymamy poprawne rozwiązanie, zwracamy sudoku.
+# - Jeśli nie otrzymamy rozwiązania, cofamy się do poprzedniego kroku i próbujemy z inną liczbą.
+# - Jeżeli wszystkie liczby od 1 do 4 zostały sprawdzone i nie otrzymaliśmy rozwiązania, zwracamy None.
+
+
